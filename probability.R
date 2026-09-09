22fdf1ec0a6a862e965659d6ffb165754a3da904
anjoliemarie <amn2902@uncw.edu>
2026-09-09 12:29:48 -0400

    Add files via upload

---
+++ probability.R
@@ -0,0 +1,32 @@
+x = c(2, 3, 6)
+mean(x)
+y = c(8,0,0)
+var(y)
+stan = c(2,3,0)
+sd(stan)
+group1 = c(6, 4, 3)
+group2 = c(3,4,2)
+cov(group1, group2)
+(-3)/15
+groupa = c(7,5,6)
+groupb = c(8,1,1)
+cor(groupa,groupb)
+
+values = c(1, 4)
+p = rep(1/2, 2)
+sum(values * p)
+
+values2 = c(7, 9)
+p2 = rep(1/2, 2)
+sum(values2 *p2)
+sum((values2^2)*p2)
+
+values3 = c(2, 1, 1)
+p3 = rep(1/3,3)
+sum(values3*p3)
+
+values4 = c(9, 2, 3)
+p4 = rep(1/3, 3)
+sum((values4^2)*p4)
+
+values5 = 

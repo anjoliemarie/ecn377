f9dd568b66181f078d938a180d6e39aeb4513087
anjoliemarie <amn2902@uncw.edu>
2026-09-15 12:41:41 -0400

    Add files via upload

---
+++ niemanaswirl4.R
@@ -0,0 +1,25 @@
+# swirl 4
+library(swirl)
+swirl()
+niemana
+1
+4
+num_vect <- c(0.5, 55, -10, 6)
+tf <- num_vect < 1
+1
+tf
+num_vect >= 6
+1
+1
+2
+my_char <- c("My", "name", "is")
+my_char
+paste(my_char, collapse = " ")
+my_name = c(my_char, "niemana")
+my_name
+paste(my_name, collapse = " ")
+paste("Hello", "world!", sep = " ")
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
+1
+0
---
+++ niemanaswirl5.R
@@ -0,0 +1,19 @@
+# swirl 5
+library(swirl)
+swirl()
+niemana
+1
+5
+x <- c(44, NA, 5, NA)
+x*3
+y <- rnorm(1000)
+z <- rep(NA, 1000)
+my_data <- sample(c(y, z), 100)
+my_na <- is.na(my_data)
+my_na
+my_data == NA
+sum(my_na)
+my_data
+0/0
+Inf - Inf
+1
